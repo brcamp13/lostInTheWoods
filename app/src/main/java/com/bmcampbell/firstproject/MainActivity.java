01fd6004095f17c3fe49e56213a5f60b65d46cde
Brandon.Campbell <brcamp13@yahoo.com>
2018-10-09 18:26:54 -0700

    Added direction array and Home Button

--- app/src/main/java/com/bmcampbell/firstproject/MainActivity.java
+++ app/src/main/java/com/bmcampbell/firstproject/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button straightButton;
     private Button rightButton;
     private Button leftButton;
+    ArrayList<String> directionList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,18 +29,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //If right, left, or straight is clicked, a message displayed to screen (for now) and corresponding direction is added to an ArrayList.
     @Override
     public void onClick(View view)
     {
         switch (view.getId()) {
             case (R.id.straightButton):
-                Toast.makeText(this, "You Clicked The Straight Button", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "You Clicked The Straight Button", Toast.LENGTH_SHORT).show();
+                directionList.add("Straight");
                 break;
             case (R.id.rightButton):
-                Toast.makeText(this, "You Clicked The Right Button", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "You Clicked The Right Button", Toast.LENGTH_SHORT).show();
+                directionList.add("Right");
                 break;
             case (R.id.leftButton):
-                Toast.makeText(this, "You Clicked The Left Button", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "You Clicked The Left Button", Toast.LENGTH_SHORT).show();
+                directionList.add("Left");
                 break;
         }
     }
